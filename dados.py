11b6b21c5fee1b548e803e6d948d88417d8e36c0
Nathália Freitas Menezes <nathaliafmenezes.ti@gmail.com>
2019-05-30 14:26:50 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,42 @@
+import pickle
+
+
+def coletar_informacoes():
+    nome = str(input('Nome: '))
+    rg = int(input('Rg:'))
+    cpf = int(input('cpf: '))
+    anoNasc = int(input('anoNasc: '))
+    mesNasc = input('mesNasc: ')
+    dia_nascimento = input('Dia_Nascimento: ')
+
+    dados = dict(name=nome, rg=rg, cpf=cpf, anoNasc=anoNasc, mesNasc=mesNasc, dia_nascimento=dia_nascimento)
+
+    return dados
+
+
+def cadastrar_pessoas():
+    try:
+        with open('dados_pessoas') as pessoas:
+            antigos_pessoas = pickle.load(pessoas)
+        with open('dados_pessoas', mode='wb') as dados_pessoas:
+            antigos_pessoas.append(coletar_informacoes())
+            novos_pessoas = pickle.dumps(antigos_pessoas)
+            dados_pessoas.write(novos_pessoas)
+    except:
+        with open('dados_pessoas', mode='wb') as dados_pessoas:
+            novos_pessoas = [coletar_informacoes()]
+            pessoas = pickle.dumps(novos_pessoas)
+            dados_pessoas.write(pessoas)
+
+
+def listar_pessoas():
+    try:
+        with open('dados_pessoas', mode='rb') as dados_pessoas:
+            lista_pessoas = pickle.load(dados_pessoas)
+            print('pessoas\n')
+            for pessoa in lista_pessoas:
+                print(pessoa['name'])
+        print('-' * 20)
+    except:
+        print('Não encontrado!')
+        menu_principal()
